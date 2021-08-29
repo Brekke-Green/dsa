7b8ea0978722dbef8ab21e8a35a003cfae8e6f67
Brekke Green <brekke.andrew.green@gmail.com>
2021-08-29 17:20:44 -0400

    Complete LeetCode - Search Insert Position Challenge

---
+++ search-insert-position.js
@@ -0,0 +1,17 @@
+// Leet Code Search Insert Position Solution - Runtime: 97.35%, Memory: 90.48%
+
+var searchInsert = function(nums, target) {
+    let left = 0;
+    let right = nums.length - 1;
+    while (left <= right) {
+        let middle = Math.floor((left + right) / 2);
+        if (nums[middle] > target) {
+            right = middle - 1
+        } else if (nums[middle] < target) {
+            left = middle + 1
+        } else {
+            return middle;
+        }
+    }
+    return Math.max(left, right);
+};
