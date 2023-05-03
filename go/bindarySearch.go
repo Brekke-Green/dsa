c11a0431b239d8069a371be21d66b260bf128b5e
Brekke-Green <brekke.andrew.green@gmail.com>
2023-05-03 16:55:55 -0400

    Complete LeetCode - Binary Search Challenge using Go

--- go/bindarySearch.go
+++ go/bindarySearch.go
@@ -2,14 +2,13 @@ func search(nums []int, target int) int {
     left := 0
     right := len(nums) - 1
     for left <= right {
-        mid := left + (right - left) / 2
-        if nums[mid] == target {
-            return mid
-        }
+        mid := (right + left) / 2
         if nums[mid] < target {
             left = mid + 1
-        } else {
+        } else if nums[mid] > target {
             right = mid - 1
+        } else {
+            return mid
         }
     }
     return -1
